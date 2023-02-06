6eaa6bfeb0de69d4fd180df1affaee22a5613c96
Yash Pratapwar <yashpratapwar@gmail.com>
2023-02-06 11:31:34 +0530

    Added basic code for backend (flask)

---
+++ app.py
@@ -0,0 +1,6 @@
+from summeet_package import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ summeet_package/models.py
@@ -0,0 +1,60 @@
+from sqlalchemy.orm import backref
+from summeet_package import db
+from flask_login import UserMixin 
+from datetime import datetime
+
+class users(db.Model, UserMixin):   
+    __tablename__ = 'users'
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    fname = db.Column(db.String(150), nullable = True)
+    lname = db.Column(db.String(150), nullable = True)
+    smh = db.Column(db.String(150), nullable = True)
+    ph_no = db.Column(db.Integer)
+    inf_email = db.Column(db.String(150), unique=True)
+    acc_handler_gender = db.Column(db.String(150), nullable = True)
+    password = db.Column(db.String(150))
+    ah_email = db.Column(db.String(150), nullable = True)
+    comp_email = db.Column(db.String(150), nullable = True, unique = True)
+    comp_website = db.Column(db.String(150), nullable = True)
+    acc_handler_desig = db.Column(db.String(150), nullable = True)
+    acc_handler_name = db.Column(db.String(150), nullable = True)
+    comp_name = db.Column(db.String(150), unique=False)
+    categories = db.Column(db.String(200), nullable=True)
+    age = db.Column(db.Integer, nullable = True)
+    gender = db.Column(db.String(150))
+    acc_type = db.Column(db.String(50))
+    infl_pic = db.Column(db.Text, nullable = True)
+    mimetype = db.Column(db.Text, nullable = True)
+    # date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    advts = db.relationship('advertisements', backref='owner') 
+
+class advertisements(db.Model, UserMixin):
+    __tablename__ = 'advertisements'
+    id  = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comp_name = db.Column(db.String(150))
+    email = db.Column(db.String(150))
+    desc = db.Column(db.String(1000))
+    brand = db.Column(db.String(150))
+    deadline = db.Column(db.Integer)
+    prdt_sp = db.Column(db.String(1000))
+    age_grp = db.Column(db.String(20))
+    name = db.Column(db.Text, nullable = False)
+    mimetype = db.Column(db.Text, nullable = False)
+    date_uploaded = db.Column(db.DateTime, default = datetime.utcnow)
+
+class advt_approval(db.Model, UserMixin):
+    __tablename__ = 'advt_approval'
+    id = db.Column(db.Integer, primary_key = True)
+    advt_id = db.Column(db.Integer, nullable = True)
+    advt_name = db.Column(db.Text, nullable = True)
+    advt_brand = db.Column(db.Text, nullable = True)
+    owner_id = db.Column(db.Integer, nullable = True)
+    owner_name = db.Column(db.Text, nullable = True)
+    infl_id = db.Column(db.Integer, nullable = True)
+    infl_fname = db.Column(db.Text, nullable = True)
+    infl_lname = db.Column(db.Text, nullable = True)
+    infl_smh = db.Column(db.Text, nullable = True)
+    infl_email = db.Column(db.Text, nullable = True)
+    approved = db.Column(db.Integer, nullable = True, default=0)
+    filter = db.Column(db.Text, nullable = True) 
