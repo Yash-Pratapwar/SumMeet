a2569a44b80ee02c02053da96d586e747144cbb9
Yash Pratapwar <yashpratapwar@gmail.com>
2023-03-09 19:54:42 +0530

    Added page loading widget and PDF generation code.

---
+++ pdf2.py
@@ -0,0 +1,28 @@
+# Python program to create
+# a pdf file
+
+
+from fpdf import FPDF
+
+
+# save FPDF() class into a
+# variable pdf
+pdf = FPDF( 'P', 'mm', 'A4' )
+
+# Add a page
+pdf.add_page()
+
+# set style and size of font
+# that you want in the pdf
+pdf.set_font("Arial", size = 15)
+
+# create a cell
+pdf.cell(175, 10, txt = "SumMeet",
+		ln = 1, align = 'R')
+
+# add another cell
+pdf.cell(200, 10, txt = "Meeting Minutes",
+		ln = 1, align = 'L')
+pdf.line_width
+# save the pdf with name .pdf
+pdf.output("GFG.pdf")
